f42b930e443260e7ed4d658307382827def53079
edensmt <epochws@gmail.com>
2019-10-09 14:52:38 -0600

    Layout complete

---
+++ app/src/main/java/com/example/mysqlproject/DbStrings.java
@@ -0,0 +1,10 @@
+package com.example.mysqlproject;
+
+public class DbStrings {
+
+	static final String DATABASE_URL = "127.0.0.1:3306";
+	static final String DATABASE_NAME = "mhs_example";
+	static final String USERNAME = "root";
+	static final String PASSWORD = "";
+
+}
